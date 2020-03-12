a824a65040b68227711972b284e5bc18d9dee9a5
Aashish Jain <aashish_jain@live.com>
2020-03-12 13:06:36 -0400

    fix dropbox oauth complete

--- src/main/java/org/onedatashare/server/service/oauth/DbxOauthService.java
+++ src/main/java/org/onedatashare/server/service/oauth/DbxOauthService.java
@@ -23,8 +23,8 @@ import java.util.Map;
 @AllArgsConstructor
 @Getter
 class DbxConfig {
-    final String clientId;
-    final String clientSecret;
+    final String key;
+    final String secret;
     final String redirectUri;
     final String identifier;
 }
@@ -40,36 +40,41 @@ public class DbxOauthService  {
     private DbxRequestConfig config;
     private DbxSessionStore sessionStore;
     private DbxWebAuth auth;
-
-    private String oAuthUrl;
+    private String token = null;
+//    private String oAuthUrl;
 
     @PostConstruct
     private void postConstructInit(){
-        secrets = new DbxAppInfo(dbxConfig.clientId, dbxConfig.clientSecret);
+        secrets = new DbxAppInfo(dbxConfig.key, dbxConfig.secret);
         config = DbxRequestConfig.newBuilder(dbxConfig.identifier).build();
         sessionStore = new DbxSessionStore() {
             public void clear() { set(null); }
-            public String get() { return dbxConfig.clientId; }
+            public String get() { return token; }
             public void set(String s) {
+                token = s;
             }
         };
 
         auth = new DbxWebAuth(config, secrets);
-        // Authorize the DbxWebAuth auth as well as redirect the user to the finishURI, done this way to appease OAuth 2.0
-        oAuthUrl = auth.authorize(DbxWebAuth
+//        // Authorize the DbxWebAuth auth as well as redirect the user to the finishURI, done this way to appease OAuth 2.0
+//        oAuthUrl = auth.authorize(DbxWebAuth
+//                .Request
+//                .newBuilder()
+//                .withRedirectUri(dbxConfig.redirectUri, sessionStore)
+//                .build());
+    }
+
+    public String start(){
+        return auth.authorize(DbxWebAuth
                 .Request
                 .newBuilder()
                 .withRedirectUri(dbxConfig.redirectUri, sessionStore)
                 .build());
     }
 
-    public String start(){
-        return oAuthUrl;
-    }
-
     public Mono<OAuthCredential> finish(String token) {
         Map<String,String[]> map = new HashMap();
-        map.put("state", new String[] {dbxConfig.clientId});
+        map.put("state", new String[] {dbxConfig.key});
         map.put("code", new String[] {token});
         try {
             DbxAuthFinish finish = auth.finishFromRedirect(dbxConfig.redirectUri, sessionStore, map);
@@ -79,7 +84,7 @@ public class DbxOauthService  {
             cred.dropboxID = account.getAccountId();
             return Mono.just(cred);
         } catch (Exception e) {
-            e.printStackTrace();
+//            e.printStackTrace();
             throw new RuntimeException(e);
         }
     }
