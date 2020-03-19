695e3205decad82760beeb3126d2a6028e4ccbb4
Aashish Jain <aashish_jain@live.com>
2020-03-18 21:11:28 -0400

    Fixed typo

--- src/main/java/org/onedatashare/server/service/oauth/DbxOauthService.java
+++ src/main/java/org/onedatashare/server/service/oauth/DbxOauthService.java
@@ -67,8 +67,8 @@ public class DbxOauthService  {
 
     public Mono<OAuthCredential> finish(Map<String, String> queryParameters) {
         Map<String,String[]> map = new HashMap();
-        map.put("state", new String[] {queryParemeters.get("state")});
-        map.put("code", new String[] {queryParemeters.get("code")});
+        map.put("state", new String[] {queryParameters.get("state")});
+        map.put("code", new String[] {queryParameters.get("code")});
         try {
             DbxAuthFinish finish = auth.finishFromRedirect(dbxConfig.redirectUri, sessionStore, map);
             OAuthCredential cred = new OAuthCredential(finish.getAccessToken());
